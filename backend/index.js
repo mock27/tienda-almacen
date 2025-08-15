0b21a5ea1a9a3023ae11252adbe7d06bb126cf88
Sidelk <urielmoctezuma.03@gmail.com>
2025-08-15 05:41:09 -0600

    Conectado a PostgreSQL en Render

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://admin:secretpassword@localhost:5432/almacen_db',
+  connectionString: process.env.DATABASE_URL || 'postgresql://sidelk:31lqR6ZXW1hfWAD6RBxdlHv5G7zjBgiw@dpg-d2fhkhje5dus73aopl4g-a.oregon-postgres.render.com/almacen_db_sl56',
   ssl: { rejectUnauthorized: false } // ¡Necesario para Render!
 });
